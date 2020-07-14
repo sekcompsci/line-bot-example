30876cd13bef6a1455b845015de376321e3fdcaf
Sirawit Moonrinta <sirawit.sek@gmail.com>
2020-07-14 14:06:07 +0700

    Add menu 3 (flex, quick reply)

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ app.post('/callback', line.middleware(config), (req, res) => {
 const handleEvent = (event) => {
     if (event.type === 'postback' && event.postback.data) return client.replyMessage(event.replyToken, { type: 'text', text: event.postback.data });
 
-    if (event.type !== 'message' || event.message.type !== 'text') return Promise.resolve(null);
+    if (event.type !== 'message' || event.type !== 'flex') return Promise.resolve(null);
 
     let message = undefined
 
@@ -151,6 +151,194 @@ const handleEvent = (event) => {
 
             break;
         }
+        case "flex": {
+            message = {
+                "type": "flex",
+                "altText": "Flex Message",
+                "contents": {
+                    "type": "bubble",
+                    "header": {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "NEWS DIGEST",
+                                "size": "sm",
+                                "weight": "bold",
+                                "color": "#AAAAAA"
+                            }
+                        ]
+                    },
+                    "hero": {
+                        "type": "image",
+                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_4_news.png",
+                        "size": "full",
+                        "aspectRatio": "20:13",
+                        "aspectMode": "cover",
+                        "action": {
+                            "type": "uri",
+                            "label": "Action",
+                            "uri": "https://linecorp.com/"
+                        }
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "spacing": "md",
+                        "contents": [
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "flex": 1,
+                                "contents": [
+                                    {
+                                        "type": "image",
+                                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_1.png",
+                                        "gravity": "bottom",
+                                        "size": "sm",
+                                        "aspectRatio": "4:3",
+                                        "aspectMode": "cover"
+                                    },
+                                    {
+                                        "type": "image",
+                                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/02_1_news_thumbnail_2.png",
+                                        "margin": "md",
+                                        "size": "sm",
+                                        "aspectRatio": "4:3",
+                                        "aspectMode": "cover"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "flex": 2,
+                                "contents": [
+                                    {
+                                        "type": "text",
+                                        "text": "7 Things to Know for Today",
+                                        "flex": 1,
+                                        "size": "xs",
+                                        "gravity": "top"
+                                    },
+                                    {
+                                        "type": "separator"
+                                    },
+                                    {
+                                        "type": "text",
+                                        "text": "Hay fever goes wild",
+                                        "flex": 2,
+                                        "size": "xs",
+                                        "gravity": "center"
+                                    },
+                                    {
+                                        "type": "separator"
+                                    },
+                                    {
+                                        "type": "text",
+                                        "text": "LINE Pay Begins Barcode Payment Service",
+                                        "flex": 2,
+                                        "size": "xs",
+                                        "gravity": "center"
+                                    },
+                                    {
+                                        "type": "separator"
+                                    },
+                                    {
+                                        "type": "text",
+                                        "text": "LINE Adds LINE Wallet",
+                                        "flex": 1,
+                                        "size": "xs",
+                                        "gravity": "bottom"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    "footer": {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "button",
+                                "action": {
+                                    "type": "uri",
+                                    "label": "More",
+                                    "uri": "https://linecorp.com"
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+
+            break;
+        }
+        case "quickreply": {
+            message = {
+                "type": "text",
+                "text": "Hello Quick Reply!",
+                "quickReply": {
+                    "items": [
+                        {
+                            "type": "action",
+                            "action": {
+                                "type": "cameraRoll",
+                                "label": "Camera Roll"
+                            }
+                        },
+                        {
+                            "type": "action",
+                            "action": {
+                                "type": "camera",
+                                "label": "Camera"
+                            }
+                        },
+                        {
+                            "type": "action",
+                            "action": {
+                                "type": "location",
+                                "label": "Location"
+                            }
+                        },
+                        {
+                            "type": "action",
+                            "imageUrl": "https://cdn1.iconfinder.com/data/icons/mix-color-3/502/Untitled-1-512.png",
+                            "action": {
+                                "type": "message",
+                                "label": "Message",
+                                "text": "Hello World!"
+                            }
+                        },
+                        {
+                            "type": "action",
+                            "action": {
+                                "type": "postback",
+                                "label": "Postback",
+                                "data": "action=buy&itemid=123",
+                                "displayText": "Buy"
+                            }
+                        },
+                        {
+                            "type": "action",
+                            "imageUrl": "https://icla.org/wp-content/uploads/2018/02/blue-calendar-icon.png",
+                            "action": {
+                                "type": "datetimepicker",
+                                "label": "Datetime Picker",
+                                "data": "storeId=12345",
+                                "mode": "datetime",
+                                "initial": "2018-08-10t00:00",
+                                "max": "2018-12-31t23:59",
+                                "min": "2018-08-01t00:00"
+                            }
+                        }
+                    ]
+                }
+            }
+
+            break;
+        }
         case "menus": {
             message = {
                 "type": "template",
@@ -167,6 +355,11 @@ const handleEvent = (event) => {
                             "type": "message",
                             "label": "Menu 2",
                             "text": "menu2"
+                        },
+                        {
+                            "type": "message",
+                            "label": "Menu 3",
+                            "text": "menu3"
                         }
                     ],
                     "title": "Menus",
@@ -240,6 +433,31 @@ const handleEvent = (event) => {
                         }
                     ],
                     "title": "Menu 2",
+                    "text": "เลือกข้อความที่ต้องการแสดง"
+                }
+            }
+
+            break;
+        }
+        case "menu3": {
+            message = {
+                "type": "template",
+                "altText": "this is a buttons template",
+                "template": {
+                    "type": "buttons",
+                    "actions": [
+                        {
+                            "type": "message",
+                            "label": "Flex",
+                            "text": "flex"
+                        },
+                        {
+                            "type": "message",
+                            "label": "Quick Reply",
+                            "text": "quickreply"
+                        }
+                    ],
+                    "title": "Menu 3",
                     "text": "เลือกข้อความที่ต้องการแสดง"
                 }
             }
